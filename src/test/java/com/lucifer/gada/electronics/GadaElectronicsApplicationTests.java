e1b51708a953eea6befa8c8df0ff51253e48a9b9
Waseem <mdvaseem014@gmail.com>
2023-04-08 22:26:32 +0530

    First Commit

---
+++ src/test/java/com/lucifer/gada/electronics/GadaElectronicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucifer.gada.electronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadaElectronicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
